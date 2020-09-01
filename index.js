590a142395bdc82c684e3c205b3694fdecba03d4
PtrckSimps <patrick_lawrence_simpao@dlsu.edu.ph>
2020-09-01 09:08:26 +0800

    Sept 1, 2020 - login, register and review

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const path = require("path")
 
 
 const app = express()
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use(express.static(path.join(__dirname, 'static')));
-
 app.set("view engine", "hbs") 
 
 const urlencoder = bodyparser.urlencoded({
@@ -31,90 +31,21 @@ app.get("/reviews", function(req, res){
     res.render('reviews.hbs')
 })
 
-app.get("/features", function(req, res){
-    res.render('features.hbs')
+app.get('/featured', (req,res) =>{
+    res.render('featured.hbs')
 })
 
-app.get("/brands", function(req, res){
-    res.render('brands.hbs')
+app.get("/logreg", function(req, res){
+    res.render('logreg.hbs')
+})
+
+app.get("/register", function(req, res){
+    res.render('register.hbs')
 })
 
 app.get("/login", function(req, res){
     res.render('login.hbs')
 })
-
-
-// app.get("/", (req, res) =>{
-//     if(req.session.username){
-//         res.render("home.hbs",{
-//             username: req.session.username
-//         })
-//     }else{
-//         res.render("index.hbs") 
-//     }    
-// })
-
-// app.post("/register", urlencoder, (req, res) =>{
-//     // username: un
-//     // password: pw
-//     let username = req.body.un
-//     let password = req.body.pw
-
-//     if(username.trim() == "" || password.trim() == ""){
-//         res.render("index.hbs", {
-//             error: "Enter a username and password"
-//         })
-//     }else if(!isAvailable(username)){
-//         res.render("index.hbs", {
-//             error: "username is already taken"
-//         })
-//     }else{
-//         req.session.username = req.body.un
-//         users.push({
-//             username: username,
-//             password: password
-//         })
-
-//         res.redirect("/")
-//     }
-// })
-
-
-// function isAvailable(username){
-//      for(let i =0; i < users.length; i++ ){
-//          if(users[i].username == username){
-//              return false
-//          }
-//      }
-//      return true
-// }
-
-// app.post("/login", urlencoder, (req, res) =>{
-    
-//     if(!matches(req.body.un, req.body.pw)){
-//         res.render("index.hbs", {
-//             error: "not match"
-//         })
-//     }else{
-//         res.render("home.hbs",{
-//             username: req.session.username
-//         })
-//     }
-// })
-
-// function matches(username, password){
-//     for(let i =0; i < users.length; i++ ){
-//         if(users[i].username == username && users[i].password == password){
-//             return true
-//         }
-//     }
-//     return false
-// }
-
-// app.get("/signout", urlencoder, (req, res) =>{
-//     req.session.destroy()
-//     res.redirect("/")
-// })
 
 app.listen(3000, ()=> { 
     console.log("Server ready")
